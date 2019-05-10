4bf2bcc2559a3ad4695795f81122032357e1c8fb
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2019-05-10 09:48:44 -0700

    MicroStructure

---
+++ Sanity.MicroStructure.js
@@ -0,0 +1,33 @@
+import elfUtils from "elven-code";
+
+describe('Git Explorer confirm client, server and micros Suite', () => {
+    it('proves we can run a test', () => {
+        expect(true).toBe(true);
+    });
+
+    it('checks if client/src/components/App.js exists', () => {
+        const fileName = __dirname + '/../client/src/components/App.js';
+        expect(elfUtils.elfUtils.fileExists(fileName)).toBe(true);
+    });
+
+    it('checks if server/app.js exists', () => {
+        const fileName = __dirname + '/../server/app.js';
+        expect(elfUtils.elfUtils.fileExists(fileName)).toBe(true);
+    });
+
+    it('checks if micros/qux/source/control.js exists', () => {
+        const fileName = __dirname + '/../micros/qux/source/control.js';
+        expect(elfUtils.elfUtils.fileExists(fileName)).toBe(true);
+    });
+
+    it('checks if micros/git-user/source/control.js exists', () => {
+        const fileName = __dirname + '/../micros/git-user/source/control.js';
+        expect(elfUtils.elfUtils.fileExists(fileName)).toBe(true);
+    });
+
+    it('checks if micros/git-gist/source/control.js exists', () => {
+        const fileName = __dirname + '/../micros/git-gist/source/control.js';
+        expect(elfUtils.elfUtils.fileExists(fileName)).toBe(true);
+    });
+
+});
